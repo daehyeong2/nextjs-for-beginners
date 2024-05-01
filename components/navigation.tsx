57d194279ce8be8f98b55a5e65523c3e6a67a231
daehyeong2 <baconbacon1231@gmail.com>
2024-05-01 20:40:10 +0900

    SSR vs CSR, Hydration (2.3~2.4)

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,10 +2,11 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Navigation = () => {
   const path = usePathname();
-  console.log(path);
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -14,6 +15,9 @@ const Navigation = () => {
         </li>
         <li>
           <Link href="/about-us">About Us</Link> {path === "/about-us" && "⭕"}
+        </li>
+        <li>
+          <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
         </li>
       </ul>
     </nav>

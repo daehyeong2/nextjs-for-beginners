96e08259fdb72850d14edbc89146c191a4f4a4ec
daehyeong2 <baconbacon1231@gmail.com>
2024-05-03 19:27:02 +0900

    Server Side (3.2)

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,18 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-const Page = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return <div>{isLoading ? "로딩 중.." : JSON.stringify(movies)}</div>;
+export const metadata = {
+  title: "Home",
 };
 
-export default Page;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+const HomePage = async () => {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+};
+
+export default HomePage;

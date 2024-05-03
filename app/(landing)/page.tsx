1adac945391bff55d879c9074319d2ab5ca6c4d8
daehyeong2 <baconbacon1231@gmail.com>
2024-05-03 18:47:47 +0900

    리액트 방식의 fetch (3.1)

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,22 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "로딩 중.." : JSON.stringify(movies)}</div>;
 };
 
 export default Page;
